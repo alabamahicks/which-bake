a58f81fd6aaa71b3807cc8c24600ad95e4016d21
Sarah Hicks <sarah.hicks@emerson.com>
2015-11-09 08:16:37 -0600

    Added jQuery, pseudocode structure

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sarahhicks on 11/9/15.
+ */
+
+//display quiz intro
+
+//close quiz intro
+
+//choose quiz level
+
+//choose quiz question
+/*display levelNumber questionNumber*/
+
+//choose answer
+/*display levelNumber questionNumber answers*/
+
+//increment score
+
+//start over
