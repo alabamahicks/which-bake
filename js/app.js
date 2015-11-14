e2fbc1eeac94f51a52af1f739c69c1d666ac48e4
Sarah Hicks <sarah.hicks@emerson.com>
2015-11-14 16:15:19 -0600

    Fleshing out variables and functions

--- js/app.js
+++ js/app.js
@@ -1,58 +1,41 @@
-/**
- * Created by sarahhicks on 11/9/15.
- */
-
-//display quiz intro
-
-//close quiz intro
-
-//choose quiz level
-
-
-//choose quiz question
-/*display levelNumber questionNumber*/
-
-//choose answer
-/*display levelNumber questionNumber answers*/
-
-//increment score
-
-//start over
-
+//How do I put this stuff in a resource file?
 
 var level1_answer1 = {
-    response: 'no response entered yet',
-    elaboration: 'no elaboration entered yet',
+    elaboration: 'CHOUX - Choux pastry has a high moisture content which allows ' +
+    'steam to force a tripling in size during baking. Its recipe is 2 parts water,' +
+    ' 1 part butter, 1 part flour, 2 parts eggs, by weight.',
     image: 'no image url entered yet'
 };
 
 var level1_answer2 = {
-    response: 'no response entered yet',
-    elaboration: 'no elaboration entered yet',
+    elaboration: 'PUFF - Puff pastry has 1:0.9 ratio of flour to butter, by weight.',
     image: 'no image url entered yet'
 };
 
 var level1_answer3 = {
-    response: 'no response entered yet',
-    elaboration: 'no elaboration entered yet',
+    elaboration: 'SHORTCRUST - Shortcrust pastry has 2:1 ratio of flour to ' +
+    'fat, by weight.',
     image: 'no image url entered yet'
 };
 
 var level1_answer4 = {
-    response: 'no response entered yet',
-    elaboration: 'no elaboration entered yet',
+    elaboration: 'ARLETTES - narrow slices of puff pastry flavored with cinnamon' +
+    ' and arranged in spirals.',
     image: 'no image url entered yet'
 };
 
 var level1_answer5 = {
-    response: 'no response entered yet',
-    elaboration: 'no elaboration entered yet',
+    elaboration: 'VOL-AU-VENTS - a small, hollow, case of puff pastry, made by' +
+    ' cutting two circles in rolled out puff, cutting a hole in one of them, then' +
+    ' stacking the ring-shaped piece on top of the disk-shaped piece.  Can be ' +
+    'savory or sweet-filled.',
     image: 'no image url entered yet'
 };
 
 var level1_answer6 = {
-    response: 'no response entered yet',
-    elaboration: 'no elaboration entered yet',
+    elaboration: 'BAKLAVA - Sometimes confused with puff pastry, phyllo/filo ' +
+    'dough also has many layers, but unlike puff, has no fat content' +
+    ' (so you brush it with butter!)',
     image: 'no image url entered yet'
 };
 
@@ -63,23 +46,91 @@ var scoreState = {
 };
 
 var level1_question1 = {
-    prompt: 'no prompt entered yet',
-    correctAnswer: level1_answer1,
+    prompt: 'Which pastry has the highest fat content?',
+    correctAnswer: level1_answer2,
     possibleAnswers: [level1_answer1, level1_answer2, level1_answer3],
     score: scoreState.UNSCORED,
     image: 'no image url entered yet'
 };
 
 var level1_question2 = {
-    prompt: 'no prompt entered yet',
-    correctAnswer: level1_answer1,
+    prompt: 'Which biscuits (cookies) are NOT made from puff pastry?',
+    correctAnswer: level1_answer3,
     possibleAnswers: [level1_answer4, level1_answer5, level1_answer6],
     score: scoreState.UNSCORED,
     image: 'no image url entered yet'
 };
 
+var currentLevel = 1;
+var currentQuestions = [];
+var userScore = 0;
+
+//on page load:
+// show quiz intro
+// hide rest of UI
+startOver();
+loadQuestions(currentLevel);
+
+//on #something click, hide quiz intro, show rest of UI
+
+//on #theme-picker li.click,
+//write index to quizLevel (default = 1)
+loadQuestions(currentLevel);
+
+//on #questions li.click, choose quiz question
+//????pass index number to displayQuestion
+//change #questions li.color = active state
+
+//on #choices li.click
+selectAnswer();
+
+//on #restart click,
+startOver();
+
+
+function displayChoices(possibleAnswers){
+    //set #choices li(each) from possibleAnswers
+}
+
+function displayQuestion(indexNumber){
+    //set #focus.background-image:url()
+    //set #focus h1.text() = chosenQuestion.prompt
+    //set #focus h2.text() = ""
+    displayChoices(chosenQuestion.possibleAnswers);
+}
+
 function displayScore(){
     // how many questions are loaded?
     // how many are correct?
-    // update UI
+    // update UI...?
 }
+
+function loadQuestions(quizLevel){
+    //set var currentQuestions to some collection of question objects
+    if (currentLevel = 1){
+        currentQuestions = [level1_question1, level1_question2];
+    }
+    //populate #questions with corresponding li count
+}
+
+function selectAnswer(choiceIndex){
+    //change li.color = active state
+    //set #focus h1.text() = question.scoredState
+    // set #focus h2.text() = answer.elaboration
+    displayScore();
+}
+
+function startOver(){
+    userScore = 0;
+    //check currentLevel
+    loadQuestions(currentLevel);
+    //set #questions li class to 'fa-question'
+}
+
+
+
+
+
+
+
+
